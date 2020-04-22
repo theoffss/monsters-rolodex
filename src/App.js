e2e699d4b6a5041b988c5b7dd0feea217dcb13dc
Jeremy Thomas <jeremythomas@confederation.fo>
2020-04-22 13:17:49 +0200

    adding starting projet

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './Components/card-list/card-list.component';
+import { SearchBox } from './Components/search-box/search-box.component';
+
+
+class App extends Component {
+constructor(){
+  super();
+
+  this.state = {
+    monsters: [    ],
+    searchField :''
+  };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response=>Response.json())
+    .then(users=>this.setState({monsters:users}));
+  }
+
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  }
+
+  render () {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+      return (
+      <div className="App">
+        <SearchBox 
+        placeholder = 'search monsters'
+        handleChange = {this.handleChange } 
+        />
+        <CardList monsters={filteredMonsters} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
