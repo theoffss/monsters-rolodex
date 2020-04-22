73c0a617beec1d688097b1c53e1c547577c8ad99
Jeremy Thomas <jeremythomas@confederation.fo>
2020-04-22 16:55:03 +0200

    Fini

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ constructor(){
       monster.name.toLowerCase().includes(searchField.toLowerCase()));
       return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox 
         placeholder = 'search monsters'
         handleChange = {this.handleChange } 
