e787bdda61f1f344f39e7b544f0c4939f539c330
Jeremy Thomas <jeremythomas@confederation.fo>
2020-04-22 17:40:27 +0200

    ds

--- src/Components/card/card.component.jsx
+++ src/Components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 
 export const Card = (props) =>(
     <div className = 'card-container'>
-    <img alt='monster' src={`http://localhost:3000/Components/card/Image/1.jpg`} />
+    <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
