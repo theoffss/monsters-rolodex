ab3a454f62864fd4df41e393f1dc7d062e8a09fe
Jeremy Thomas <jeremythomas@confederation.fo>
2020-04-22 18:41:07 +0200

    Updates

---
+++ precache-manifest.b0b7953adb8046c680e317d359fcbb6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c2ce814ba01baccfe71038bb0ea792",
+    "url": "/io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8446fd99646696468d5",
+    "url": "/io/monsters-rolodex/static/css/main.2549d60b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8446fd99646696468d5",
+    "url": "/io/monsters-rolodex/static/js/main.1d965e10.chunk.js"
+  },
+  {
+    "revision": "beea5fee1a554168393c",
+    "url": "/io/monsters-rolodex/static/js/runtime-main.640ef8cf.js"
+  }
+]);
